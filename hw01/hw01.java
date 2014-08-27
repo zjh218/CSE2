661833655b82ea907dd80c144d9ab82df950012f
zjh218 <zjh218@lehigh.edu>
2014-08-27 16:22:37 +0000

    hw01

---
+++ hw01/hw01.java
@@ -0,0 +1,17 @@
+// define a class
+public class hw01{
+
+    // add main mathod
+    public static void main(String[] args) {
+     // print the statement with the box around it
+     System.out.println("-----------");
+     System.out.println("| WELCOME |");
+     System.out.println("-----------");
+     System.out.println("^  ^  ^  ^  ^  ^");
+     System.out.println("|-Z--J--H--2--1--8-|");
+     System.out.println("v  v  v  v  v  v");
+     System.out.println("This summer, I did absoloutely nothing productive.");
+     
+     //test/testest
+     }
+}
