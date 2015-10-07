b0c823a6d68e84f4914741a38e0e8aeebb95d155
zjh218 <zjh218@lehigh.edu>
2015-10-07 02:35:52 +0000

    wh yoh god why

--- hw06/pyramid.java
+++ hw06/pyramid.java
@@ -7,28 +7,20 @@ public class pyramid{
         int j = 0;
         int k = 0;
         size = getInt(scan);//used getInt method to find pyramid siz
-        int r = (int)(size-1);
+        int r = (int)(size);
         int q = 0;//weird counter
-        for (int i = 0; i <= size; i++){
-            //# of lines,
-                    for(int z = r; z >= 0; z--){
-                        System.out.print(" ");
-                    }
-            for (j = 0; j < (i); j++){
-                for ( k = (j); k < (j+1); k++){
+        int p;
+        int z = 1;
+        for (int i = 0; i < size; i++){
+            p = size-i;
+            for (j = 0; j < (p); j++){
+             System.out.print(' ');
+            }
+                for ( k = 0; k < (z); k++){
                 System.out.print('*');
-                }
             }
+            z=z+2;
             System.out.println("");
-            if ( (q%2) == 0){
-                r--;
-                q++;
-               // System.out.println("iF");
-            }
-            else {
-              //  System.out.println("else");//for checking where my loop lay
-                q++;
-            }
         }
 
     }
@@ -38,7 +30,7 @@ public class pyramid{
                     return scan.nextInt();//return input integer
                 }
             System.out.println("That wasn't an option. Try again");//or keep asking for better input
-            scan.next();
+            scan.next();//clear scanner
         }
         
     }
